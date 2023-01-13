d791e870e2de1d35b77534e272c6a159cf3606f5
therealwheel3 <114943445+therealwheel3@users.noreply.github.com>
2023-01-13 16:58:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 import pygame
+import math
+import random
+
+
+class projectile:
+    def __init__(self, x, y, r, color):
+        self.x = x
+        self.y = y
+        self.v = (random.randint(0, 1), random.randint(0, 5), random.randint(0, 5))
+        self.r = r
+        self.color = color
+    
+    def render(self):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
+    
+    def kill(self, other):
+        a, b, c = (self.x, self.y, self.r)
+        d, e, f = (other.x, other.y, other.r)
+        if math.sqrt((a - d) ** 2 + (b - e) ** 2) > c + f:
+            return False
+        return True
+
+    def move(self):
+        if self.v[0] == 0:
+            self.x -= self.v[1]
+        else:
+            self.x += self.v[1]
+        self.y += self.v[-1]
+            
 
 class P_bullet:
     def __init__(self, x, y):
@@ -41,8 +70,8 @@ class Screen:
     def __init__(self):
         pass
 
-
-
+a = projectile(400, 200, 19, (0, 0, 0))
+projectiles = []
 bullets = []
 player = Player()
 if __name__ == '__main__':
@@ -58,6 +87,8 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.MOUSEBUTTONUP:
+                player.shot()
+            if keys[pygame.K_SPACE]:
                 player.shot()
             elif keys[pygame.K_s] and keys[pygame.K_d]:
                 player.move(1, 1)
@@ -78,6 +109,16 @@ if __name__ == '__main__':
         for i in bullets:
             i.render()
             i.move()
+            if i.y < 0:
+                del bullets[bullets.index(i)]
+        for i in projectiles:
+            if 0 > i.x > 800 or 0 > i.y > 600:
+                del projectiles[projectiles.index(i)]
+                print(projectiles)
+            i.render()
+            i.move()
+            if i.kill(player):
+                print(1)
         player.render()
         pygame.display.flip()
         clock.tick(Screen.FPS)
